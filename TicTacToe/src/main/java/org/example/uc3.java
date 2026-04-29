2090e8ff16921f6b93647e65d3888d42ddb2fe20
saichaitanya4974 <sm9121@srmist.edu.in>
2026-04-29 09:16:00 +0530

    added featurres/uc3

---
+++ TicTacToe/src/main/java/org/example/uc3.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+    public class uc3 {
+        public static void main(String[] args) {
+            int slot = getUserSlot();
+            System.out.println("Slot entered: " + slot);
+        }
+
+        static int getUserSlot() {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter slot number (1-9): ");
+            int slot = sc.nextInt();
+
+            return slot;
+        }
+    }
+
