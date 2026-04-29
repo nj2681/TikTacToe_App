40d38bb8f5acf980324744de6551c1e2dc122e30
saichaitanya4974 <sm9121@srmist.edu.in>
2026-04-29 09:45:32 +0530

    added uc7

---
+++ TicTacToe/src/main/java/org/example/uc7.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class uc7
+{
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
+
+    public static void main(String[] args) {
+        computerMove();
+        printBoard();
+    }
+
+    static void computerMove() {
+        Random random = new Random();
+
+        while (true) {
+            int slot = random.nextInt(9) + 1;
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                System.out.println("Computer placed O in slot " + slot);
+                break;
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
